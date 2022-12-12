0bbdc33f8bdd8112707e474c9f733f97c3780742
pharmony01 <rileypeters2016@gmail.com>
2022-12-12 18:47:56 -0500

    Getting rid of old methods

--- pokemon.py
+++ pokemon.py
@@ -274,28 +274,6 @@ def neural_network(data):
     plt.show()
     
 
-'''Desciption: Calculates the HP stat for the pokemon
-Inputs: base stat, iv, ev, level, gen
-Output: HP stat as an int'''
-def hp(base, iv, ev, level, gen):
-    if gen >= 3:
-        return math.floor(((2 * base + iv + math.floor(ev/4)) * level)/100) + level + 10
-    else:
-        return math.floor((((base + iv) * 2 + math.floor(math.ceil(math.sqrt(ev))/4)) * level)/100) + level + 10
-
-'''Desciption: Calculates any stat for the pokemon other than HP
-Inputs: base stat, iv, ev, level, gen, nature (2: benifits stat, 1: netural, 0: hinders stat)
-Output: Stat as an int'''
-def stat(base, iv, ev, level, gen, nature):
-    if gen >= 3:
-        if nature == 2: nature = 1.1
-        elif nature == 1: nature = 1
-        else: nature = 0.9
-
-        return math.floor((math.floor(((2 * base + iv + math.floor(ev/4)) * level) / 100) + 5) * nature)
-    else:
-        return math.floor((((base + iv) * 2 + math.floor(math.ceil(math.sqrt(ev))/4)) * level)/100) + 5
-
 '''Desciption: Splits the given data set into a training and testing set given a set percentage 
 Inputs: data, training split percentage (as a decimal), shuffle boolean (whether or not the indexs it picks are random or just in order)
 Output: An array for all the training indexes and an array for all the testing indexes'''
